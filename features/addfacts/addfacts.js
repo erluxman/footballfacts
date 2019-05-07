85089b6da5d7d8e6550affa4f205897a97cbbf53
Laxman Bhattarai <erluxman@gmail.com>
2019-05-07 13:50:27 +0545

    initial commit

---
+++ features/addfacts/addfacts.js
@@ -0,0 +1,124 @@
+class Todo {
+    constructor(work, done) {
+        this.work = work;
+        this.done = done;
+    }
+}
+
+
+class DB {
+
+    static getAlltasks() {
+
+        let tasks;
+        if (localStorage.getItem('tasks') === null) {
+            tasks = []
+        } else {
+            tasks = JSON.parse(localStorage.getItem('tasks'))
+        }
+
+        return tasks;
+    }
+    static getDoneTasks() {
+
+        return DB.getAlltasks().filter(item => {
+            return item.done === true
+        })
+
+    }
+
+    static getNotDoneTasks() {
+        return DB.getAlltasks().filter((item) => {
+            return item.done === false
+        })
+    }
+    static addTask(task) {
+        const tasks = DB.getAlltasks()
+        tasks.push(task)
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
+
+    static deleteTask(work) {
+        const tasks = DB.getAlltasks()
+        tasks.forEach((task, index) => {
+            if (work == task.work) {
+                tasks.splice(index, 1)
+            }
+        })
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
+
+    static updateTask(work, completed) {
+        console.log("task to update " + work);
+        const tasks = DB.getAlltasks()
+        tasks.forEach((task, index) => {
+            if (work == task.work) {
+                tasks.splice(index, 1)
+            }
+        })
+        tasks.push(new Todo(work, completed))
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
+}
+
+class UI {
+    static displayNotDoneTasks() {
+        DB.getNotDoneTasks().forEach((task) => UI.addNotDonetask(task))
+    }
+
+    static addNotDonetask(task) {
+        const list = document.querySelector('#todo-list')
+        const row = document.createElement('tr')
+        row.innerHTML = `
+        <td class="not-done-work-text">${task.work}</td>
+        <td><i class="far fa-trash-alt delete-task"></i></td>
+        <td><i class="far fa-check-circle not-done-mark"></i></td>
+        `
+        list.appendChild(row)
+    }
+
+    static addDoneTask(task) {
+        const list = document.querySelector('#done-list')
+        const row = document.createElement('tr')
+        row.innerHTML = `
+        <td class="done-work-text">${task.work}</td>
+        <td><i class="far fa-trash-alt delete-task"></i></td>
+        <td><i class="far fa-check-circle done-mark"></i></td>
+        `
+        list.appendChild(row)
+    }
+    static displayDoneTasks() {
+        DB.getDoneTasks().forEach((task) => UI.addDoneTask(task))
+    }
+
+    static removeTask(work) {
+        console.log(work)
+        work.parentElement.parentElement.remove()
+    }
+
+    static clearInput() {
+        document.querySelector("#todo-field").value = ""
+    }
+
+    static showAlert(message, className) {
+        const alert = document.querySelector('#message-dialog');
+        alert.className = `alert alert-${className}`;
+        const messageChild = document.createTextNode(message);
+        alert.appendChild(messageChild)
+        setTimeout(
+            () => {
+                alert.removeChild(messageChild);
+                alert.className = `hidden`
+            }
+            , 1500)
+    }
+}
+
+document.querySelector("#todo-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const work = document.querySelector("#todo-field").value
+    const todo = new Todo(work, false)
+    DB.addTask(todo);
+    UI.showAlert("Successfully Added", "success")
+    UI.clearInput()
+})
