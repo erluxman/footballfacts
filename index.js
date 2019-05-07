b3e85b171fb02364f1cbc7ad84377c175d8b83b4
Laxman Bhattarai <erluxman@gmail.com>
2019-05-07 19:02:13 +0545

    cleaned up home page

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ class DB {
 
         let tasks;
         if (localStorage.getItem('tasks') === null) {
-            tasks = []
+            tasks = [];
         } else {
             tasks = JSON.parse(localStorage.getItem('tasks'));
         }
@@ -23,7 +23,7 @@ class DB {
 
         return DB.getAlltasks().filter(item => {
             return item.done === true;
-        })
+        });
 
     }
 
@@ -32,50 +32,9 @@ class DB {
             return item.done === false;
         });
     }
-    static addTask(task) {
-        const tasks = DB.getAlltasks();
-        tasks.push(task);
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
-
-    static deleteTask(work) {
-        const tasks = DB.getAlltasks();
-        tasks.forEach((task, index) => {
-            if (work == task.work) {
-                tasks.splice(index, 1);
-            }
-        });
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
-
-    static updateTask(work, completed) {
-        console.log("task to update " + work);
-        const tasks = DB.getAlltasks();
-        tasks.forEach((task, index) => {
-            if (work == task.work) {
-                tasks.splice(index, 1);
-            }
-        });
-        tasks.push(new Todo(work, completed))
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
 }
 
 class UI {
-    static displayNotDoneTasks() {
-        DB.getNotDoneTasks().forEach((task) => UI.addNotDonetask(task));
-    }
-
-    static addNotDonetask(task) {
-        const list = document.querySelector('#todo-list');
-        const row = document.createElement('tr');
-        row.innerHTML = `
-        <td class="not-done-work-text">${task.work}</td>
-        <td><i class="far fa-trash-alt delete-task"></i></td>
-        <td><i class="far fa-check-circle not-done-mark"></i></td>
-        `;
-        list.appendChild(row);
-    }
 
     static addDoneTask(task) {
         const list = document.querySelector('#done-list');
@@ -85,56 +44,26 @@ class UI {
         <td><i class="fas fa-thumbs-up like"></i></td>
         <td><i class="fas fa-thumbs-down dislike"></i></td>
         `;
-        list.appendChild(row)
+        list.appendChild(row);
     }
     static displayDoneTasks() {
-        DB.getDoneTasks().forEach((task) => UI.addDoneTask(task))
-    }
-
-    static removeTask(work) {
-        console.log(work)
-        work.parentElement.parentElement.remove()
-    }
-
-    static clearInput() {
-        document.querySelector("#todo-field").value = "";
-    }
-
-    static showAlert(message, className) {
-        const alert = document.querySelector('#message-dialog');
-        alert.className = `alert alert-${className}`;
-        const messageChild = document.createTextNode(message);
-        alert.appendChild(messageChild)
-        setTimeout(
-            () => {
-                alert.removeChild(messageChild);
-                alert.className = `hidden`;
-            }, 1500);
+        DB.getDoneTasks().forEach((task) => UI.addDoneTask(task));
     }
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
     UI.displayDoneTasks();
-    UI.displayNotDoneTasks();
 });
 
-document.querySelector("#todo-list").addEventListener('click', handleDelete)
-document.querySelector("#done-list").addEventListener('click', handleDelete)
+document.querySelector("#done-list").addEventListener('click', handleDelete);
 
 function handleDelete(e) {
     const classList = e.target.classList;
-    if (classList.contains('done-mark')) {
-        UI.removeTask(e.target);
+    if (classList.contains('like')) {
+        /* UI.removeTask(e.target);
         UI.addNotDonetask(new Todo(e.target.parentElement.previousElementSibling.previousElementSibling.textContent, true));
         DB.updateTask(e.target.parentElement.previousElementSibling.previousElementSibling.textContent, false);
-        UI.showAlert("Undo Complete", "success");
-    } else if (classList.contains('not-done-mark')) {
-        UI.removeTask(e.target);
-        UI.addDoneTask(new Todo(e.target.parentElement.previousElementSibling.previousElementSibling.textContent, true));
-        DB.updateTask(e.target.parentElement.previousElementSibling.previousElementSibling.textContent, true);
-        UI.showAlert("Completed", "success");
-    }
-    else {
-
+        UI.showAlert("Undo Complete", "success"); */
+    } else if (classList.contains('dislike')) {
     }
 }
